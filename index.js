1a8aec6257c799569453ff8554ed8f508c52d4a4
Michael Louie Loria <michaellouieloria@gmail.com>
2021-01-14 12:30:10 -0600

    initial graphql setup

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import schema from './schema';
+import { graphqlHTTP } from 'express-graphql';
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Graphql is amazing!');
 })
+
+const root = { hello: () => "Hello, I'm Michael" };
+
+app.use('/graphql', graphqlHTTP ({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
+
 
 app.listen(8080, () => console.log('Running on server port localhost:8080/graphql'));
