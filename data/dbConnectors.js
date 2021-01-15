c7381ae6678d80bbea16120307e32151af99230f
Michael Louie Loria <michaellouieloria@gmail.com>
2021-01-15 13:08:39 -0600

    data persistence with sql

--- data/dbConnectors.js
+++ data/dbConnectors.js
@@ -1,4 +1,7 @@
 import mongoose, { mongo } from 'mongoose';
+import Sequelize from 'sequelize';
+import _ from 'lodash';
+import casual from 'casual';
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://192.168.1.221:27017/friends', {
@@ -32,4 +35,22 @@ const friendSchema = new mongoose.Schema({
 
 const Friends = mongoose.model('friends', friendSchema);
 
-export { Friends };
+// SQL
+const sequelize = new Sequelize('database', null, null, {
+  dialect: 'sqlite',
+  storage: './alien.sqlite'
+});
+
+const Aliens = sequelize.define('aliens', {
+  firstName: {
+    type: Sequelize.STRING
+  },
+  lastName: {
+    type: Sequelize.STRING
+  },
+  planet: {
+    type: Sequelize.STRING
+  },
+});
+
+export { Friends, Aliens };
